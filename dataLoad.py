d57f29911ce481b336bc4804ffcc8e8e7639d0be
Cihan Tugrul TEZCAN <c.tugrultezcan@gmail.com>
2015-10-26 12:32:18 +0300

    ANGORA
    ANGORA (Analyzing the Period Changes of Eclipsing Binary Stars), Çift
    yıldızların ışık eğrilerinden elde edilen minimum zamanlarının
    analizini içeren bir paket programdır.
    
    ANGORA is a package astronomical program for analysing changes of
    binary stars.

---
+++ dataLoad.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 26 12:30:02 2015
+
+@author: CTugTEZ
+
+ANGORA: DESCRIPTION: ANGORA (Analyzing the Period Changes of Eclipsing Binary Stars), 
+Çift yıldızların ışık eğrilerinden elde edilen minimum zamanlarının analizini i
+çeren bir paket programdır. 
+
+"""
+
+
+#This is file loading page. File includes minimum times, observers, filters and methods.
+
+
